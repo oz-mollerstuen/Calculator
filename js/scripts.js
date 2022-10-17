5e47a4879a27e30184c62f6c8fe62cfffc80802b
Student <student@epicodus-5.hsd1.or.comcast.net>
2022-10-17 14:09:20 -0700

    Add math functions and output
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,29 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function times(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function math(num1, num2) {
+  const addNum = `The addition of ${num1} and ${num2} equals:` + add(num1, num2);
+  const subNum = `The subtraction of ${num1} and ${num2} equals: ` + sub(num1, num2);
+  const timesNum = `The multipication of ${num1} and ${num2} equals: ` + times(num1, num2);
+  const divNum = `The division of ${num1} and ${num2} equals: ` + div(num1, num2);
+  window.alert(addNum + "\n" + subNum + "\n" + timesNum + "\n" + divNum);
+}
+
 // Interface logic
 
 const num1 = parseInt(prompt("Enter a number: "));
 const num2 = parseInt(prompt("Enter another number: "));
 
-window.alert(add(num1, num2));
+math(num1, num2);
