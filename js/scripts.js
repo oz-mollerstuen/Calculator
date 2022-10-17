0dace46c0602ad56c2111f151e61344feb011476
Student <student@epicodus-5.hsd1.or.comcast.net>
2022-10-17 14:34:16 -0700

    Add formatting
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,11 @@ function div(num1, num2) {
 }
 
 function math(num1, num2) {
-  const addNum = `The addition of ${num1} and ${num2} equals:` + add(num1, num2);
-  const subNum = `The subtraction of ${num1} and ${num2} equals: ` + sub(num1, num2);
-  const timesNum = `The multipication of ${num1} and ${num2} equals: ` + times(num1, num2);
-  const divNum = `The division of ${num1} and ${num2} equals: ` + div(num1, num2);
-  window.alert(addNum + "\n" + subNum + "\n" + timesNum + "\n" + divNum);
+  const addNum = `The addition of ${num1} and ${num2} equals:  ${add(num1, num2)}`;
+  const subNum = `The subtraction of ${num1} and ${num2} equals:  ${sub(num1, num2)}`;
+  const timesNum = `The multipication of ${num1} and ${num2} equals:  ${times(num1, num2)}`;
+  const divNum = `The division of ${num1} and ${num2} equals:  ${div(num1, num2)}`;
+  window.alert(`${addNum}\n${subNum}\n${timesNum}\n${divNum}`);
 }
 
 // Interface logic
